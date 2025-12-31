a731f0958d8dc61cb04f9e8f9ad2aee734b968e0
Evelin Aasna <evelin.aasna@fu-berlin.de>
2025-12-31 13:16:18 +0100

    Add switch for uniform or normally distributed sampling

--- src/generate_local_matches.cpp
+++ src/generate_local_matches.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <random>
 #include <ranges>
 
@@ -23,12 +24,39 @@ struct cmd_arguments
     uint32_t max_match_length{200u};
     uint32_t total_num_matches{1ULL<<20};
     uint64_t ref_len;
+    bool uniform{true};
+    bool normal{false};
+    double std_dev_fraction{0.1};
+
     bool reverse{false};
     uint32_t seed{42};
 
     bool verbose_ids{false};
 };
 
+template <typename func_t>
+void runtime_to_compile_time(func_t const & func, bool b1)
+{
+    if (b1) 
+        func.template operator()<true>();
+    else
+        func.template operator()<false>();
+}
+
+template <typename dist_T>
+dist_T get_match_start_dis(uint64_t const seq_size, cmd_arguments const & arguments)
+{
+	if (std::is_same<dist_T, std::uniform_int_distribution<uint64_t>>::value)
+		return dist_T{0, seq_size - arguments.max_match_length};
+	else
+	{
+		const uint64_t seq_mean = std::round((seq_size - arguments.max_match_length) / 2.0);
+        	// seqan3::debug_stream << "seq_mean\t" << seq_mean << '\n'; 
+		return dist_T{(double) seq_mean, seq_mean * arguments.std_dev_fraction};
+	}
+}
+
+template <bool is_uniform>
 void run_program(cmd_arguments const & arguments)
 {
     std::random_device rd;
@@ -49,19 +77,26 @@ void run_program(cmd_arguments const & arguments)
     uint32_t match_counter{};
     auto sample_matches = [&](auto const & seq, auto const & reference_name, uint32_t const & num_matches, bool const reverse = false)
     {
-        uint64_t const seq_len = std::ranges::size(seq);
         uint32_t per_seq_matches = num_matches;
         if (seq.size() < arguments.ref_len - 1)  // if more than one chromosome
-            per_seq_matches = std::round(num_matches * (double) seq_len / (double) arguments.ref_len);
-        
-        seqan3::debug_stream << "Simulating " << per_seq_matches << " matches from sequence " << reference_name << '\n'; 
+	{
+	    //seqan3::debug_stream << "seq.size()\t" << seq.size() << '\n';
+	    //seqan3::debug_stream << "arguments.ref_len\t" << arguments.ref_len  << '\n';
+	    //seqan3::debug_stream << "num_matches\t" << num_matches << '\n';
+	    per_seq_matches = std::round(num_matches * (double) seq.size() / (double) arguments.ref_len);
+	}
+        //seqan3::debug_stream << "Simulating " << per_seq_matches << " matches from sequence " << reference_name << '\n'; 
+	using start_dis_T = std::conditional<is_uniform, 
+	      				     std::uniform_int_distribution<uint64_t>, 
+	      				     std::normal_distribution<double>>::type;
+	start_dis_T match_start_dis = get_match_start_dis<start_dis_T>(seq.size(), arguments);
 
-        std::uniform_int_distribution<uint64_t> match_start_dis(0, seq_len - arguments.max_match_length);
-        for (uint32_t current_match_number = 0; current_match_number < num_matches; ++current_match_number, ++match_counter)
+	for (uint32_t current_match_number = 0; current_match_number < num_matches; ++current_match_number, ++match_counter)
         {
             uint32_t match_length = match_len_dis(rng);
             std::uniform_int_distribution<uint32_t> match_error_position_dis(0, match_length - 1);
-            uint64_t const match_start_pos = match_start_dis(rng);
+            // uint64_t const match_start_pos = match_start_dis(rng);
+            uint64_t const match_start_pos = std::clamp<uint64_t>(std::round(match_start_dis(rng)), 0,  seq.size() - arguments.max_match_length);
             std::vector<seqan3::dna4> match;
             for (auto & n : seq | seqan3::views::slice(match_start_pos, match_start_pos + match_length))
                 match.push_back(n);
@@ -93,6 +128,8 @@ void run_program(cmd_arguments const & arguments)
             }
             matches.emplace_back(match, query_id + meta_info);
         }
+
+	//seqan3::debug_stream << "emplaced_back matches\n";
     };
 
     for (auto const & [seq, reference_name] : fref)
@@ -110,9 +147,13 @@ void run_program(cmd_arguments const & arguments)
         }
     }
 
+    //seqan3::debug_stream << "reversed\n";
+
     seqan3::sequence_file_output fout_matches{arguments.matches_out_path};
     for (auto & match : matches)
         fout_matches.push_back(match);
+
+    //seqan3::debug_stream << "pushed_back\n";
 
     if (!arguments.query_path.empty())
     {
@@ -225,6 +266,16 @@ void initialise_argument_parser(seqan3::argument_parser & parser, cmd_arguments 
                       "ref-len",
                       "Length of the reference.",
 		              seqan3::option_spec::required);
+    parser.add_flag(arguments.normal,
+                    '\0',
+                    "normal",
+                    "Local alignment positions are normally distributed across the sequence. \n"
+		    "Default: uniform distribution of start positions.");
+    parser.add_option(arguments.std_dev_fraction,
+		    '\0',
+		    "std-dev-frac",
+		    "Set the standard deviation of the normal distribution as a fraction of the mean."
+		    "Default: std dev 10% of the mean.");
     parser.add_flag(arguments.reverse,
                     'r',
                     "reverse",
@@ -254,7 +305,13 @@ int main(int argc, char ** argv)
         return -1;
     }
 
-    run_program(arguments);
+    if (arguments.normal)
+        arguments.uniform = false;
+
+    runtime_to_compile_time([&]<bool is_uniform>()
+    {
+        run_program<is_uniform>(arguments);
+    }, arguments.uniform);
 
     return 0;
 }
